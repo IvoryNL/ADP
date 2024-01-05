75a4c0870bb3d23b1c32646c6592ac307874d67f
Ivan Franken <ivanfranken@royalfloraholland.com>
2024-01-05 02:08:34 +0100

    Added implementation of Doubly Ended Queue

---
+++ ADP/ADP/Datastructures/DoublyEndedQueue/DoubleEndedQueue.cs
@@ -0,0 +1,106 @@
+﻿namespace ADP.Datastructures.DoublyEndedQueue
+{
+    public class DoubleEndedQueue<T>
+    {
+        private int _count;
+        private Node<T>? _head;
+        private Node<T>? _tail;
+
+        public DoubleEndedQueue()
+        {
+            _count = 0;
+            _head = null;
+            _tail = null;
+        }
+
+        public void InsertLeft(T data)
+        {
+            var newNode = new Node<T>(data);
+
+            if (_head == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                _head.Previous = newNode;
+                newNode.Next = _head;
+                _head = newNode;
+            }
+
+            _count++;
+        }
+
+        public T DeleteLeft() 
+        {
+            if (_head == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            var data = _head.Data;
+
+            if (_head.Next == null)
+            {
+                _head = null;
+                _tail = null;
+            }
+            else
+            {
+                _head = _head.Next;
+            }
+
+            _count--;
+
+            return data;
+        }
+
+        public void InsertRight(T data)
+        {
+            var newNode = new Node<T>(data);
+
+            if (_tail == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                _tail.Next = newNode;
+                newNode.Previous = _tail;
+                _tail = newNode;
+            }
+
+            _count++;
+        }
+
+        public T DeleteRight()
+        {
+            if (_tail == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            var data = _tail.Data;
+
+            if (_tail.Previous == null)
+            {
+                _head = null;
+                _tail = null;
+            }
+            else
+            {
+                _tail = _tail.Previous;
+            }
+
+            _count--;
+            return data;
+        }
+
+        public int Size()
+        {
+            return _count;
+        }
+    }
+}
