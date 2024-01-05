cad49a36106b40b898d2ebcd58b24481d14b1a76
Ivan Franken <ivanfranken@royalfloraholland.com>
2024-01-05 02:20:05 +0100

    Added implementaton of a Quick Sort

---
+++ ADP/ADP/Algorithms/Quick Sort/QuickSortAlgorithm.cs
@@ -0,0 +1,44 @@
+﻿namespace ADP.Algorithms.QuickSort
+{
+    public static class QuickSortAlgorithm
+    {
+        public static void QuickSort(int[] inputCollection, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            var pivot = Partition(inputCollection, start, end);
+            QuickSort(inputCollection, start, pivot - 1);
+            QuickSort(inputCollection, pivot + 1, end);
+        }
+
+        private static int Partition(int[] inputCollection, int start, int end)
+        {
+            int j = start - 1;
+            int pivot = inputCollection[end];
+            int temp;
+
+            for (int i = start; i < end; i++)
+            {
+                if (inputCollection[i] < pivot)
+                {
+                    j++;
+
+                    temp = inputCollection[i];
+                    inputCollection[i] = inputCollection[j];
+                    inputCollection[j] = temp;
+                }
+            }
+
+            j++;
+
+            temp = inputCollection[j];
+            inputCollection[j] = pivot;
+            inputCollection[end] = temp;
+
+            return j;
+        }
+    }
+}
