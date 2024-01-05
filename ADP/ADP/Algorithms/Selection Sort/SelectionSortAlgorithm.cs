ec60062a06200b5b459e22ab42603fc769a9c669
Ivan Franken <ivanfranken@royalfloraholland.com>
2024-01-05 02:17:59 +0100

    Added implementation of a Selection Sort

---
+++ ADP/ADP/Algorithms/Selection Sort/SelectionSortAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿namespace ADP.Algorithms.SelectionSort
+{
+    public class SelectionSortAlgorithm
+    {
+        public static int[] SelectionSort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                var minValue = inputArray[i];
+                var currentIndex = i + 1;
+
+                for (int j = currentIndex; j < inputArray.Length; j++)
+                {
+                    if (inputArray[j] < minValue)
+                    {
+                        minValue = inputArray[j];
+                        currentIndex = j;
+                    }
+                }
+
+                if (minValue < inputArray[i])
+                {
+                    var temp = inputArray[i];
+                    inputArray[i] = inputArray[currentIndex];
+                    inputArray[currentIndex] = temp;
+                }
+            }
+
+            return inputArray;
+        }
+    }
+}
