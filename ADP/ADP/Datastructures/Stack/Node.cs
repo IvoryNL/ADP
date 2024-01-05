4dc35111485740ebb1387eb41af56fccb02ea0d6
Ivan Franken <ivanfranken@royalfloraholland.com>
2024-01-05 02:07:26 +0100

    Added implementation of Stack

---
+++ ADP/ADP/Datastructures/Stack/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace ADP.Datastructures.Stack
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T>? Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data ?? throw new ArgumentNullException();
+        }
+    }
+}
---
+++ ADP/ADP/Datastructures/Stack/Stack.cs
@@ -0,0 +1,62 @@
+﻿namespace ADP.Datastructures.Stack
+{
+    public class Stack<T>
+    {
+        private int _count;
+        private Node<T>? _head;
+
+        public Stack()
+        {
+            _count = 0;
+            _head = null;
+        }
+
+        public void Push(T data)
+        {
+            var newNode = new Node<T>(data);
+
+            if (_head == null)
+            {
+                _head = newNode;
+            }
+
+            newNode.Next = _head;
+            _head = newNode;
+            _count++;
+        }
+
+        public T Pop()
+        {
+            if (_head == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            var data = _head.Data;
+            _head = _head.Next;
+            _count--;
+
+            return data;
+        }
+
+        public T Top()
+        {
+            if (_head == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return _head.Data;
+        }
+
+        public int Size()
+        {
+            return _count;
+        }
+
+        public bool IsEmpty()
+        {
+            return _count > 0;
+        }
+    }
+}
