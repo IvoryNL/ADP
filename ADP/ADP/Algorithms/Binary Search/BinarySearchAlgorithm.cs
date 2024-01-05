0717f009c66d37bb3cea50e2d5054d352a1bbfd0
Ivan Franken <ivanfranken@royalfloraholland.com>
2024-01-05 02:14:36 +0100

    Added implementation of a Binary Search

---
+++ ADP/ADP/Algorithms/Binary Search/BinarySearchAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿namespace ADP.Algorithms.Binary_Search
+{
+    public static class BinarySearchAlgorithm
+    {
+        public static int BinarySearch(List<int> sortedList, int numberToFind)
+        {
+            var left = 0;
+            var right = sortedList.Count - 1;
+
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+
+                if (numberToFind == sortedList[mid])
+                {
+                    return mid;
+                }
+
+                if (numberToFind < sortedList[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
