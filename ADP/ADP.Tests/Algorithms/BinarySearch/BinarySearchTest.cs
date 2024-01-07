6eaa62e4ce82f3bb356b99b6cb70d6a23a897e55
Ivan Franken <ivanfranken@royalfloraholland.com>
2024-01-07 21:38:37 +0100

    Refactor Unit Tests Binary Search

--- ADP/ADP.Tests/Algorithms/BinarySearch/BinarySearchTest.cs
+++ ADP/ADP.Tests/Algorithms/BinarySearch/BinarySearchTest.cs
@@ -8,51 +8,45 @@ namespace ADP.Tests.Algorithms.BinarySearch;
 public class BinarySearchTest
 {
     private readonly ITestOutputHelper _testOutputHelper;
-    private const int COUNT_SMALL_ORDERED_LIST = 1000;
-    private const int COUNT_MEDIUM_ORDERED_LIST = 10000;
-    private const int COUNT_LARGE_ORDERED_LIST = 100000;
-    
-    private static List<int> smallOrderedList = new();
-    private static List<int> mediumOrderedList = new();
-    private static List<int> largeOrderedList = new();
-
     public BinarySearchTest(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
-        for (int i = 1; i <= COUNT_SMALL_ORDERED_LIST; i++)
-        {
-            smallOrderedList.Add(i);
-        }
-        
-        for (int i = 1; i <= COUNT_MEDIUM_ORDERED_LIST; i++)
-        {
-            mediumOrderedList.Add(i);
-        }
-        
-        for (int i = 1; i <= COUNT_LARGE_ORDERED_LIST; i++)
-        {
-            largeOrderedList.Add(i);
-        }
     }
     
     [Theory]
-    [MemberData(nameof(Data))]
+    [MemberData(nameof(Data1))]
+    [MemberData(nameof(Data2))]
+    [MemberData(nameof(Data3))]
+    [MemberData(nameof(Data4))]
     public void TestBinarySearch(List<int> input, int numberToFind)
     {
+        // Arrange
         var stopwatch = new Stopwatch();
-        _testOutputHelper.WriteLine("Stopwatch start");
+        
+        // Act
         stopwatch.Start();
         var result = BinarySearchAlgorithm.BinarySearch(input, numberToFind);
         stopwatch.Stop();
-        _testOutputHelper.WriteLine("Stopwatch stop");
+        
         _testOutputHelper.WriteLine($"Elapsed time {stopwatch.Elapsed}");
         
-        result.ShouldBeEquivalentTo(numberToFind);
+        // Assert
+        result.ShouldBeEquivalentTo(numberToFind - 1);
     }
     
-    public static IEnumerable<object[]> Data(){
-        yield return new object[] { smallOrderedList, 990 };
-        yield return new object[] { mediumOrderedList, 9990 };
-        yield return new object[] { largeOrderedList, 99990 };
+    public static IEnumerable<object[]> Data1(){
+        yield return new object[] { Enumerable.Range(1, 10).ToList(), 1 };
+    }
+    
+    public static IEnumerable<object[]> Data2(){
+        yield return new object[] { Enumerable.Range(1, 100000).ToList(), 1 };
+    }
+    
+    public static IEnumerable<object[]> Data3(){
+        yield return new object[] { Enumerable.Range(1, 10000000).ToList(), 1 };
+    }
+    
+    public static IEnumerable<object[]> Data4(){
+        yield return new object[] { Enumerable.Range(1, 2000000000).ToList(), 1 };
     }
 }
