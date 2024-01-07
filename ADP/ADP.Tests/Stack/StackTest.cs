ad49171ac09d0c47836b95c1707b0d8ea7bb7037
Ivan <ivan.franken89@outlook.com>
2024-01-07 16:15:31 +0100

    Added Stack Unit Tets

---
+++ ADP/ADP.Tests/Stack/StackTest.cs
@@ -0,0 +1,59 @@
+﻿using Shouldly;
+
+namespace ADP.Tests.Stack;
+
+public class StackTest
+{
+    private readonly CustomStack.Stack<int> _stack = new();
+
+    public StackTest()
+    {
+        foreach (var item in DataSetSorterenHelper.LijstOplopend10000)
+        {
+            _stack.Push(item);
+        }
+    }
+
+    [Fact]
+    public void TestPush()
+    {
+        // Arrange
+        var valueToPush = 30;
+
+        // Act
+        _stack.Push(valueToPush);
+
+        // Assert
+        var result = _stack.Top();
+        
+        result.ShouldBeEquivalentTo(valueToPush);
+    }
+
+    [Fact]
+    public void TestPop()
+    {
+        // Arrange
+        var topValue = _stack.Top();
+
+        // Act
+        var result = _stack.Pop();
+ 
+        // Assert
+        result.ShouldBeEquivalentTo(topValue);
+        _stack.Top().ShouldNotBe(topValue);
+    }
+
+    [Fact]
+    public void TestTop()
+    {
+        // Arrange
+        var valueToPush = 40;
+        _stack.Push(valueToPush);
+        
+        // Act
+        var result = _stack.Top();
+        
+        // Assert
+        result.ShouldBeEquivalentTo(valueToPush);
+    }
+}
